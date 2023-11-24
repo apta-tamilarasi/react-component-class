132842c9344f8705784ced42b33e981fc9f45fb4
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-11-24 10:02:43 +0530

    addnew

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Class from './Component/Class.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Class/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Component/Class.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+class Class extends Component {
+    render() {
+        return <div>
+            <h2>Class Component</h2>
+        </div>
+    }
+
+}
+export default Class
